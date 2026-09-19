471778387ea20554cfd4354d7cddf9da4d29258b
Morfeo <gastongomez2014@hotmail.com>
2026-09-19 17:11:36 -0300

    Landing pages

--- src/contexts/LanguageContext.tsx
+++ src/contexts/LanguageContext.tsx
@@ -530,8 +530,8 @@ export const LanguageProvider = ({ children }: { children: ReactNode }) => {
           {
             title: "Landing Page",
             image: SimpleImage3,
-            tools: ["HTML", "CSS"],
-            link: "https://huddle-landing-page-morfeo1997.netlify.app/",
+            tools: ["Typescript", "CSS", "Tailwindcss"],
+            link: "https://landing-collection.netlify.app",
           },
           {
             title: "TODO App (with dark mode)",
